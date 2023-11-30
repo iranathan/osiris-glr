33a909160ada7a4b28a11a5ac5d0401aea43c57a
ira <iranathan8@gmail.com>
2023-11-30 21:38:40 +0100

    misclick

--- objects/personalia.js
+++ objects/personalia.js
@@ -1,11 +1,12 @@
 // Represents a Personalia of a user.
 export default class Personalia {
     constructor(data) {
-        this.name = `${data.roepnaam} ${data.acternaam}`;
+        this.name = `${data.roepnaam} ${data.achternaam}`;
         this.role = data.rol;
         this.gender = data.geslacht;
         this.studentNumber = data.studentnummer;
         this.firstName = data.roepnaam;
         this.age = data.leeftijd;
+        this.photo = data.pasfoto;
     }
 }
